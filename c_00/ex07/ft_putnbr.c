63ed73089c200f73627e3f830be4616547552833
Frederico Hallamman Silva Freitas <fhallamm@c1r19s10.42lisboa.com>
2022-08-20 14:52:21 +0100

    add

---
+++ c_00/ex07/ft_putnbr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fhallamm <fhallamm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/13 21:21:02 by fhallamm          #+#    #+#             */
+/*   Updated: 2022/08/13 22:34:21 by fhallamm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	print(char c)
+{
+	write(1, &c, 1);
+}
+
+void	condicao(int nb)
+{
+	if (nb < 10)
+	{
+		print(nb + 48);
+		return ;
+	}
+	else
+	{
+		condicao(nb / 10);
+	}
+	condicao(nb % 10);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		print('-');
+		print('2');
+		nb = 147483648;
+	}
+	if (nb < 0)
+	{
+		print('-');
+		nb = nb * -1;
+	}
+	condicao(nb);
+}
